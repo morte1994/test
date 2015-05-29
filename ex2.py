3fc1a2636821f433313756ddb8fdf473ff946c1c
morte1994 <1328087534@qq.com>
2015-05-30 01:27:37 +0800

    ex2
    ex2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-  
+__author__ = 'ss-pc'
+# A comment,this is so you read your program later.
+#anything after the# is ignored by python.
+
+print"I could have code like this." #and the comment after is ignored
+
+#you can also use a comment to"disable" or comment out a piece piece of code:
+#print "This won't run."
+print"this will run."
