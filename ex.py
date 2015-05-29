51c3b59fbb0c8d945b0eb074c7996fc0969dd7cb
morte1994 <1328087534@qq.com>
2015-05-30 01:25:08 +0800

    test
    add a python

---
+++ ex.py
@@ -0,0 +1,19 @@
+
+x = "there are %d types of people" % 10
+binary = "binary"
+do_not = "don't"
+y = "those who know %s and those who %s" % (binary, do_not)
+
+print x,y
+
+print "I said : %r." % x
+print "I also said : '%s'" %y
+
+hil = False
+joke_eva = "Isn't that joke so funny? %r"
+print joke_eva % hil
+
+w = "This is the left side of"
+e = "a string with a right side"
+
+print w+e
