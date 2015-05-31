c304f0921b800ab86b11af01ef9444686f4250e9
morte1994 <1328087534@qq.com>
2015-05-31 23:33:35 +0800

    practice python oob
    practice python oob

---
+++ ex4402.py
@@ -0,0 +1,11 @@
+__author__ = 'ss-pc'
+class Parent(object):
+    def override(self):
+        print "PARENT override()"
+class Child(Parent):
+    def override(self):
+        print "CHILD override()"
+dad = Parent()
+son = Child()
+dad.override()
+son.override()
