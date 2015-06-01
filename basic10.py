592dff2b3969da20ef32b9036258f38030fd2afc
morte1994 <1328087534@qq.com>
2015-06-01 23:15:50 +0800

    vamei practice basic1-10
    from  blog vamei practice

---
+++ basic10.py
@@ -0,0 +1,4 @@
+dic = {'lilei': 90, 'lily': 100, 'sam': 57, 'tom': 90}
+for key in dic:
+    print dic[key]
+    print key
---
+++ basic_python.py
@@ -0,0 +1,12 @@
+__author__ = 'ss-pc'
+
+b = [1, 2, 3]
+def chan(b):
+    b[0] = b[0] + 1
+    return b
+print(chan(b))
+print(b)
+
+print("-"*50)
+print [1,2,3] + [5,6,9]
+
